48add6841a29158b7ad36537d7c172273241656f
ativie-btech <favour.a@blutechsolutions.com>
2024-11-17 16:57:06 +0100

    Change of origin to accept nginx domain

--- src/start/startup.js
+++ src/start/startup.js
@@ -1,37 +1,30 @@
-require("dotenv-flow").config()
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { router } = require('../routes/main.route');
-const { responseHandler } = require('../handlers/response.handler');
-const { pool } = require('../database/database.config');
-const {errorMiddleware}= require("../handlers/middlewareError.handler")
+require("dotenv-flow").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { router } = require("../routes/main.route");
+const { responseHandler } = require("../handlers/response.handler");
+const { pool } = require("../database/database.config");
+const { errorMiddleware } = require("../handlers/middlewareError.handler");
 
 const app = express();
 // Middleware for CORS, JSON parsing, and cookies
-app.use(cors({
-  origin: 'http://localhost:8080',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8080",
+      "https://client-portal-v1.onrender.com",
+      "https://nginx-configuration-4f3p.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 
-
-
-
-
-
-
-//Error handlers and database connection
-/* app.use(async(err, req, res, next)=>{
-    const client = await pool.connect()
-   
-    req.client = client
-    next()
-}); */
-app.use('/api/v1/products', router);
+app.use("/vendor_service/api/v1/products", router);
 app.use(errorMiddleware.globalErrorHandler);
-app.all('*', errorMiddleware.globalNotFoundHandler);
+app.all("*", errorMiddleware.globalNotFoundHandler);
 
 module.exports = { app };
